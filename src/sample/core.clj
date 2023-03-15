7cc39ad6152d994b5771158a74d23c01b823b388
Lucas Arthur <luca.oetdbem@gmail.com>
2023-03-15 19:23:59 -0300

    add CORS middleware/add CORS env config

--- src/sample/core.clj
+++ src/sample/core.clj
@@ -6,21 +6,21 @@
    [web.commons.middleware.exception :refer [wrap-exceptions]]
    [web.commons.middleware.metrics :refer [wrap-metrics]]
    [web.commons.middleware.swagger :refer [wrap-swagger]]
+   [web.commons.middleware.cors :refer [wrap-cors]]
    [sample.routes :refer [routes]]
    [sample.swagger :refer [swagger]]
-   [environ.core :refer [env]]
-   [ring.middleware.defaults :refer [wrap-defaults api-defaults]])
+   [environ.core :refer [env]])
   (:gen-class))
 
 (def api
   (-> routes
+      (wrap-cors)
       (wrap-exceptions)
       (log-http-requests {:on-kafka? true})
       (wrap-metrics)
-      (wrap-defaults api-defaults)
       (wrap-swagger swagger)))
 
 (defn -main []
-  (start-server! api {:port (-> env :port Integer/parseInt)
+  (start-server! api {:port (-> env :port parse-long)
                       :on-start #(log-on-kafka ::server-started :port (port))
                       :on-shutdown #(log-on-kafka ::server-shutdown)}))
